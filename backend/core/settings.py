c02b5420453fb0ee624f7f4cc325586ddc375a2a
puskarpok <puskarkumarpokharel@gmail.com>
2026-03-08 11:25:40 +0545

    Fix database connection to SQLite and fix build dependencies

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -84,11 +84,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
